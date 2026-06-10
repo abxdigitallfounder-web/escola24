ce352e4c402364aff4cf8196d12049e062d3c591
abxdigitallfounder-web <abxdigitall.founder@gmail.com>
2026-06-10 15:01:57 -0300

    Update checkout URL in ProgramDetails component

--- src/pages/ProgramDetails.tsx
+++ src/pages/ProgramDetails.tsx
@@ -95,7 +95,7 @@ const ProgramDetails: React.FC = () => {
 			}
 			setShowSuccess(true);
 			setTimeout(() => {
-				window.location.href = "https://checkout.inscricao-agentescoladofuturo.online/VCCL1O8SCK8R?utm_source=FB&utm_campaign={{campaign.name}}|{{campaign.id}}&utm_medium={{adset.name}}|{{adset.id}}&utm_content={{ad.name}}|{{ad.id}}&utm_term={{placement}}";
+				window.location.href = "https://seguro.agentescoladofuturo.org/checkout/Z-10SQC06N9Q2633011?utm_source=FB&utm_campaign={{campaign.name}}|{{campaign.id}}&utm_medium={{adset.name}}|{{adset.id}}&utm_content={{ad.name}}|{{ad.id}}&utm_term={{placement}}";
 			}, 4000);
 		}
 	};
